0f3e62c4b1b1d9c37531e58e778029116392c887
FebrianAditya <febrian.aditya@student.upi.edu>
2025-06-23 02:28:57 +0700

    add story2 ParkingAttendant

--- index.js
+++ index.js
@@ -90,9 +90,63 @@ class ParkingLot {
 }
 
 
+// Story3 dan Story4
+
+class ParkingAttendant {
+    constructor(name) {
+        this.name = name
+        this.parkingLots = new Map()
+    }
+
+    // Menambahkan parking lot ke dalam list yang dikelola petugas
+    addParkingLot(parkingLot) {
+        this.parkingLots.set(parkingLot.id, parkingLot);
+    }
+
+    // Menentukan lot yang tersedia (delegasi pengambilan keputusan)
+    chooseAvailableLot() {
+        let manipulateArray =  Array.from(this.parkingLots.values())
+        console.log();
+        
+        let result = manipulateArray.find(lot => !lot.isFull())
+        return result
+    }
+
+    // Mengecek apakah suatu mobil dengan plat tertentu sudah terparkir di manapun
+    isCarParked(plateNumber) {
+        
+        let result = this.parkedCars.has(plateNumber);
+        console.log(result, "==> ahaha");
+        return result
+    }
+
+    // Memarkirkan mobil, pastikan mobil belum terparkir sebelumnya (menghindari double parking)
+    park(car) {
+        const lot = this.chooseAvailableLot();
+        if (!lot) {
+            throw new Error("Semua tempat parkir penuh");
+        }
+        
+        return lot.park(car);
+    }
+
+
+    // Menampilkan daftar lot yang dikelola oleh petugas
+    showHandledLots() {
+        let result = Array.from(this.parkingLots.values());
+        // console.log(result[2].id);
+        return result   
+    }
+}
+
+
+
+
+
+// Story 1 dan 2
+
 // Instance tempat parkir dan buat kapasitas
 const lotA = new ParkingLot("A", 3);
-
 
 // Skenario 1 Honda Civic B1234XYZ
 // Instance Mobil yang akan Prkir
@@ -125,3 +179,44 @@ const suzukiApv = lotA.park(car2)
 let cekParkir = lotA.cekPark()
 // console.log(cekParkir);
 
+
+
+// Story 3 dan 4
+const attendantBudi = new ParkingAttendant("Pak Budi");
+const lotB = new ParkingLot("B", 1);
+const lotC = new ParkingLot("C", 2);
+
+// Skenario 1
+// tambah lot yang dikelola pak Budi
+attendantBudi.addParkingLot(lotA)
+attendantBudi.addParkingLot(lotB)
+attendantBudi.addParkingLot(lotC)
+
+
+// Skenario 2
+// add mobil di parkiran
+const toyotaKijang = new Car("T1234KS", "MPV", "Toyota Kijang")
+const ticket2 = attendantBudi.park(toyotaKijang)
+// console.log("Petugas memarkirkan mobil:", ticket2);
+const toyotaVios = new Car("AB555K", "sedan", "Toyota Vios")
+const ticket3 = attendantBudi.park(toyotaVios)
+// console.log("Petugas memarkirkan mobil:", ticket3);
+const wulingConfiro = new Car("B111OK", "MPV", "Wuling Confiro")
+const ticket4 = attendantBudi.park(wulingConfiro)
+// console.log("Petugas memarkirkan mobil:", ticket4);
+const mazda2 = new Car("T805FEB", "Sedan", "Mazda2")
+const ticket5 = attendantBudi.park(mazda2)
+// console.log("Petugas memarkirkan mobil:", ticket5);
+
+
+// Skenario 3
+// Lihat lot yang dikelola Pak Budi setelah ada yang parkir
+let lotsPakBudi = attendantBudi.showHandledLots()
+console.log(lotsPakBudi);
+
+
+// Skenario 4
+// Parkiran Penuh
+// const toyotaAvanza = new Car("T909FEB", "MPV", "Toyota Avanza")
+// const ticket6 = attendantBudi.park(toyotaAvanza)
+// console.log("Petugas memarkirkan mobil:", ticket6);
